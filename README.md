7d781e7d2c1c3c9eb8376bbc784456b3927f2ad4
Rodrigo Quelhas <r.quelhas@papers.ch>
2022-06-19 21:30:01 +0100

    feat: Improves the bot structure, including calculations and logging

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The [youves](https://youves.com) frontend does not support manual step-in directly. Consequently, manual step-ins have to be done through a block explorer, such as Better Call Dev (BCD), or with a bot that triggers when certain conditions are met and interacts directly with the smart contracts.
 
-This repository contains a bot implementation for automating the liquidation tasks.
+This repository contains a bot implementation for automating the liquidation process.
 
 ## Install dependencies
 
@@ -17,7 +17,7 @@ The bot configuration is included in file [settings.env](./settings.env). You sh
 The configurations can also be provisioned when [calling the bot script](#run-the-bot):
 
 ```sh
-DEBUG=True python liquidation_bot.py
+LOG_LEVEL=20 python liquidation_bot.py
 ```
 
 #### **Configuration Values**
@@ -29,13 +29,22 @@ DEBUG=True python liquidation_bot.py
 | **`TZKT_ENDPOINT`** | *`https://api.tzkt.io/v1`* | TZKT API endpoint to be used by the bot. |
 | **`ENGINE_ADDRESS`** | *`KT1FFE2LC5JpVakVjHm5mM36QVp2p3ZzH4hH`* | Address of the engine contract. |
 | **`TARGET_PRICE_ORACLE_ADDRESS`** | *`KT1C5zJ62ZY3sm88XSznawP9ExHogzGUuqDr`* | Address of the price oracle contract. |
+| **`COLLATERAL_TOKEN_DECIMALS`** | *`6`* | Position of the decimal point in collateral token balances. |
 | **`TOKEN_DECIMALS`** | *`12`* | Position of the decimal point in token balances. |
-| **`EMERGENCY_RATIO`** | *`0.2`* | [Emergency collateral ratio](https://docs.youves.com/syntheticAssets/stableTokens/collateralManagement/Collateral-Management-Details). |
-| **`MINIMUM_REWARD`** | *`0.05`* | Minimum expected reward from liquidations (in ꜩ). |
-| **`DEBUG`** | *`True`*                      | Flag to enable stack straces to appear in the logs. |
+| **`EMERGENCY_RATIO`** | *`200`* | [Emergency collateral ratio](https://docs.youves.com/syntheticAssets/stableTokens/collateralManagement/Collateral-Management-Details). |
+| **`MINIMUM_REWARD`** | *`0.1`* | Minimum expected reward from liquidations (in ꜩ). |
+| **`LOG_LEVEL`** | *`10`*                      | ERROR = 40, INFO = 20, DEBUG = 10 |
 
 ## Run the bot
 
 ```sh
 python liquidation_bot.py
 ```
+
+## Supported Youves Engines
+
+| Engine Address | Token Ticker | Collateral Ticker | Target Collateral Ratio | Emergency Collateral Ratio |
+|------------|:-------:|:-------:|:-------:|:-------|
+| **KT1FFE2LC5JpVakVjHm5mM36QVp2p3ZzH4hH** | uUSD | XTZ | 300% | 200% |
+| **KT1LQcsXGpmLXnwrfftuQdCLNvLRLUAuNPCV** | uDEFI | XTZ | 300% | 200% |
+| **KT1VjQoL5QvyZtm9m1voQKNTNcQLi5QiGsRZ** | uBTC | XTZ | 300% | 200% |
--- liquidation_bot.py
+++ liquidation_bot.py
@@ -1,6 +1,6 @@
-import traceback
 import requests
 import logging
+from decimal import Decimal
 from pytezos import pytezos, Key
 import settings
 from view_utils import get_oracle_price
@@ -11,39 +11,45 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(settings.LOG_LEVEL)
 
-PRICE_PRECISION_FACTOR = 10**6
-LIQUIDATION_REWARD_BITSHIFT = 3
+TARGET_COLLATERAL_RATIO = 3 # 300%
 
 class LiquidationBot():
     """
     A bot that verifies if vaults are open to step-ins and liquidates them.
 
     Attributes:
+        :param name: Bot identifier.
         :param rpc_endpoint: RPC node uri.
         :param private_key: A private key to sign bot operations.
         :param tzkt_endpoint: TZKT API uri.
         :param engine_address: Address of the engine contract.
         :param oracle_address: Address of the price oracle contract.
+        :param collateral_token_decimals: Position of the decimal point in collateral token balances.
         :param token_decimals: Position of the decimal point in token balances.
         :param emergency_ratio: Emergency collateral ratio.
         :param minimum_reward: Minimum expected reward from liquidations.
     """
     def __init__(
         self,
+        name: str,
         rpc_endpoint: str,
         private_key: str,
         tzkt_endpoint: str,
         engine_address: str,
         oracle_address: str,
-        token_decimals: str,
+        collateral_token_decimals: int,
+        token_decimals: int,
         emergency_ratio: float,
         minimum_reward: float
     ):
+        self.name = name
         self.tzkt_endpoint = tzkt_endpoint
-        self.emergency_ratio = emergency_ratio
-        self.minimum_reward = minimum_reward
+        self.emergency_ratio = Decimal(emergency_ratio)
+        self.minimum_reward = Decimal(minimum_reward)
+        self.token_decimals = Decimal(token_decimals)
+        self.collateral_token_decimals = Decimal(collateral_token_decimals)
 
         # Create a PyTezos client instance
         self.client = pytezos.using(shell=rpc_endpoint, key=Key.from_encoded_key(key=private_key))
@@ -55,8 +61,6 @@ class LiquidationBot():
         self.token = self.client.contract(self.engine.storage["token_contract"]())
         self.token_id = self.engine.storage["token_id"]()
 
-        self.token_precison_factor = 10 ** int(token_decimals)
-
         # Set initial block timestamp to 0.
         # It will be updated inside the `run` method.
         self.previous_now = 0
@@ -67,48 +71,67 @@ class LiquidationBot():
         """
         try:
             if self.has_new_head():
-                # Fetch the latest price from the oracle
-                oracle_price = self.oracle_price()
-
-                compound_interest_rate = self.engine.storage['compound_interest_rate']()
+                # Cache values
+                target_price = self.oracle_price()
+                token_balance = self.token_balance()
+                tez_balance = self.client.balance()
+                compound_interest_rate = Decimal(self.engine.storage['compound_interest_rate']())
 
                 did_any_liquidation = False
                 for vault in self.vaults():
-                    minted = int(vault["value"]["minted"])
-                    balance = int(vault["value"]["balance"])
                     is_being_liquidated = vault["value"]["is_being_liquidated"]
+                    minted = Decimal(vault["value"]["minted"])
+                    balance = Decimal(vault["value"]["balance"])
 
-                    # Compute engine ratio (This will change in engine v3)
-                    engine_ratio = (oracle_price * compound_interest_rate / 10**24) * self.emergency_ratio
-                    # Skip if emergency ratio has not been reached
-                    # and if vault is not being liquidated
-                    if (not is_being_liquidated and (minted == 0 or float(balance / minted) > engine_ratio)):
+                    # The vault must have some token amount minted
+                    if minted == 0:
                         continue
 
-                    # Compute the amount to be liquidated
-                    liquidation_threshold = int(1.6 * ((minted*compound_interest_rate/self.token_precison_factor) - (balance/3*(self.token_precison_factor/oracle_price)))) - PRICE_PRECISION_FACTOR
-                    amount_to_liquidate = min(liquidation_threshold, self.token_balance())
-                    tez_to_receive = self.liquidation_reward_in_tez(amount_to_liquidate, oracle_price)
+                    token_factor = 10**self.token_decimals
+                    minted_synthetic_asset = minted * compound_interest_rate / token_factor
+                    collateral_ratio = (balance / minted_synthetic_asset / target_price) * token_factor * 100
 
-                    if tez_to_receive > self.minimum_reward:
-                        print()
-                        self.log(f"Liquidating {vault['key']}")
-                        self.log(f"Amount being liquidated: {amount_to_liquidate / self.token_precison_factor}")
-                        self.log(f"Liquidation reward: {tez_to_receive}")
-                        self.liquidate_vault(vault["key"], amount_to_liquidate)
-                        print()
-                        did_any_liquidation = True
+                    # Step-in is only allowed when the collateral ratio is less or equal
+                    # to 200%, or if the vault is already being liquidated.
+                    if not is_being_liquidated and collateral_ratio > self.emergency_ratio:
+                        continue
+
+                    self.debug(f"Found vault '{vault['key']}' with collateral ratio ({(collateral_ratio):.2f}%). (is_being_liquidated: {is_being_liquidated})")
+
+                    # Compute the amount to be liquidated
+                    amount_to_liquidate = self.amount_to_liquidate(balance, minted_synthetic_asset, target_price)
+                    amount_to_liquidate = int(min(amount_to_liquidate, token_balance))
+                    tez_to_receive = self.liquidation_reward_in_tez(amount_to_liquidate, target_price)
+
+                    if tez_to_receive >= self.minimum_reward:
+                        self.info(f"Liquidating {vault['key']}")
+                        self.info(f"Amount to liquidate: {amount_to_liquidate}")
+                        self.info(f"Liquidation reward: {tez_to_receive:.5f} ꜩ")
+                        err = self.liquidate_vault(vault["key"], amount_to_liquidate)
+                        if err is None:
+                            self.info(f"Tez balance before liquidation: {tez_balance} ꜩ")
+                            tez_balance = self.client.balance()
+                            self.info(f"Tez balance after liquidation: {self.client.balance()} ꜩ")
+
+                            self.info(f"Token balance before liquidation: {token_balance}")
+                            token_balance = self.token_balance()
+                            self.info(f"Token balance after liquidation: {token_balance}\n")
+
+                            # Set to True to inform that at least one liquidation was successful
+                            did_any_liquidation = True
+                        else:
+                            self.error(f"Liquidating failed with: {err}.")
+                    else:
+                        self.debug(f"Ignoring liquidation, amount ({tez_to_receive:.5f} ꜩ) it too low...")
 
                 if not did_any_liquidation:
-                    self.log("Nothing to do...")
+                    self.debug("Nothing to do...")
 
                 # Set a new high water mark
                 self.previous_now = self.now()
 
         except Exception as ex:
-            self.log(f"Something went wrong: {ex}.")
-            if settings.DEBUG:
-                traceback.print_exc()
+            self.error(f"Something went wrong: {ex}.")
 
     def vaults(self):
         """
@@ -117,30 +140,55 @@ class LiquidationBot():
         vaults = requests.get(f"{self.tzkt_endpoint}/contracts/{self.engine.address}/bigmaps/vault_contexts/keys?limit=10000")
         return vaults.json()
 
-    def liquidation_reward_in_tez(self, token_amount, target_price):
+    def amount_to_liquidate(
+        self,
+        balance: Decimal,
+        minted_synthetic_asset: Decimal,
+        target_price: Decimal
+    ) -> Decimal:
         """
-        Compute liquidation reward.
-        """
-        token_amount_market_value = token_amount * target_price
-        liquidation_reward = token_amount_market_value >> LIQUIDATION_REWARD_BITSHIFT
-        return ((token_amount_market_value + liquidation_reward) // self.token_precison_factor) / PRICE_PRECISION_FACTOR
+        Compute the amount to liquidate.
 
-    def liquidate_vault(self, vault_owner, amount_to_liquidate):
+        Attributes:
+            :param balance: Collateral amount in the vault.
+            :param minted_synthetic_asset: Minted amount in the vault.
+            :param target_price: Quote price.
+        """
+        collateral_token_factor = 10**self.collateral_token_decimals
+        token_factor = 10**self.token_decimals
+        excess_minted_amount = (minted_synthetic_asset - (balance / TARGET_COLLATERAL_RATIO * (token_factor / target_price))) - collateral_token_factor
+        # 1.6 <=> (1 + step bonus)
+        return excess_minted_amount * Decimal(1.6)
+
+    def liquidation_reward_in_tez(self, amount_to_liquidate: Decimal, target_price: Decimal) -> Decimal:
+        """
+        Compute liquidation reward in ꜩ.
+
+        Attributes:
+            :param amount_to_liquidate: Synthetic token amount to be liquidated.
+            :param target_price: Quote price.
+        """
+        bonus = Decimal(1.125) # TODO (Maybe this constant can be fetched from somewhere)
+        return (amount_to_liquidate * target_price * bonus) / (10**(self.token_decimals+self.collateral_token_decimals))
+
+    def liquidate_vault(self, vault_owner: str, amount_to_liquidate: Decimal):
         """
         Perform a liquidation on a vault.
+
+        Attributes:
+            :param vault_owner: The vault owner.
+            :param amount_to_liquidate: Synthetic token amount to be liquidated.
         """
         try:
-            self.log(f"Balance before liquidation: {self.client.balance()}")
             self.engine.liquidate(vault_owner=vault_owner, token_amount=amount_to_liquidate).send(min_confirmations=1)
-            self.log(f"Balance after liquidation: {self.client.balance()}")
         except Exception as ex:
-            self.log(f"Liquidating failed with: {ex}.")
+            return ex
 
-    def token_balance(self):
+    def token_balance(self) -> Decimal:
         """
         Get the current token amount owned by this account.
         """
-        return self.token.balance_of(
+        balance = self.token.balance_of(
             requests = [
                 {
                     'owner'     : self.public_key_hash,
@@ -149,12 +197,13 @@ class LiquidationBot():
             ],
             callback = None
         ).callback_view()[0]['balance']-1
+        return Decimal(balance)
 
-    def oracle_price(self) -> int:
+    def oracle_price(self) -> Decimal:
         """
         Get the latest price provisioned in the oracle contract.
         """
-        return get_oracle_price(self.oracle)
+        return Decimal(get_oracle_price(self.oracle))
 
     def has_new_head(self) -> bool:
         """
@@ -168,27 +217,29 @@ class LiquidationBot():
         """
         return self.client.now()
 
-    def log(self, message) -> None:
-        """
-        Log bot information.
+    def info(self, message) -> None:
+        logger.info("%s\t%s", self.name, message)
 
-        Attributes:
-            :param message: Message to log.
-        """
-        logger.info("%s\t%s", self.engine.address, message)
+    def debug(self, message) -> None:
+        logger.debug("%s\t%s", self.name, message)
+
+    def error(self, message) -> None:
+        logger.error("%s\t%s", self.name, message)
 
 
-bot__uUSD_tez = LiquidationBot(
-    tzkt_endpoint   = settings.TZKT_ENDPOINT,
-    rpc_endpoint    = settings.RPC_ENDPOINT,
-    private_key     = settings.PRIVATE_KEY,
-    engine_address  = settings.ENGINE_ADDRESS,
-    oracle_address  = settings.TARGET_PRICE_ORACLE_ADDRESS,
-    token_decimals  = settings.TOKEN_DECIMALS,
-    emergency_ratio = settings.EMERGENCY_RATIO,
-    minimum_reward  = settings.MINIMUM_REWARD
+bot = LiquidationBot(
+    name                        = settings.ENGINE_ADDRESS, # Use engine address as bot name (Used in the logs)
+    tzkt_endpoint               = settings.TZKT_ENDPOINT,
+    rpc_endpoint                = settings.RPC_ENDPOINT,
+    private_key                 = settings.PRIVATE_KEY,
+    engine_address              = settings.ENGINE_ADDRESS,
+    oracle_address              = settings.TARGET_PRICE_ORACLE_ADDRESS,
+    collateral_token_decimals   = settings.COLLATERAL_TOKEN_DECIMALS,
+    token_decimals              = settings.TOKEN_DECIMALS,
+    emergency_ratio             = settings.EMERGENCY_RATIO,
+    minimum_reward              = settings.MINIMUM_REWARD
 )
 
-bot__uUSD_tez.log("Bot Initialized...")
+bot.info("Initialized...")
 while True:
-    bot__uUSD_tez.run()
+    bot.run()
